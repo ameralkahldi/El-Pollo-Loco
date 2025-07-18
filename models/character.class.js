550173d88f8a454bd5ea1d4a10106c7740c8d0c5
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-07-18 14:00:58 +0200

    Move Character

--- models/character.class.js
+++ models/character.class.js
@@ -2,17 +2,37 @@ class Character extends MovableObject {
   y = 235;
   height = 200;
 
+  IMAGE_WALKTING=[
+     "/img/2_character_pepe/2_walk/W-21.png",
+      "/img/2_character_pepe/2_walk/W-22.png",
+      "/img/2_character_pepe/2_walk/W-23.png",
+      "/img/2_character_pepe/2_walk/W-24.png",
+      "/img/2_character_pepe/2_walk/W-25.png",
+      "/img/2_character_pepe/2_walk/W-26.png"
+  ];
+
+  currentImage=0;
+
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
-    this.loadImages([
-      "img/2_character_pepe/2_walk/W-21.png",
-      "img/2_character_pepe/2_walk/W-22.png",
-      "img/2_character_pepe/2_walk/W-23.png",
-      "img/2_character_pepe/2_walk/W-24.png",
-      "img/2_character_pepe/2_walk/W-25.png",
-      "img/2_character_pepe/2_walk/W-26.png"
-    ]);
+    this.loadImages(this.IMAGE_WALKTING);
+    this.animate();
   }
+
+
+  
+  animate() {
+    setInterval(() => {
+      let i= this.currentImage % this.IMAGE_WALKTING.length;
+      let path= this.IMAGE_WALKTING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 1000);
+
+
+  }
+
+
 
   jump() {}
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -11,14 +11,14 @@ class MovableObject {
     this.img.src = path;
    }
 
-   
+
    loadImages(arr) {
-      arr.forEach((path) => {
-         let img = new Image();
-         img.src = path;
-        this.imageCache[path]=path;
-      });
-   }
+  arr.forEach((path) => {
+    let img = new Image();
+    img.src = path;
+    this.imageCache[path] = img;
+  });
+}
 
 
    moveRight(){
